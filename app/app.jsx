db1bfd59e0ea058f6107952ca890d50dd9d3e4d4
llernestal <lars.lernestal@gmail.com>
2015-10-08 11:54:31 +0200

    Added store tom main app script

--- app/app.jsx
+++ app/app.jsx
@@ -5,9 +5,18 @@ import 'normalize.css/normalize.css';
 import './scss/app.scss';
 
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/App/App';
+import reducer from './reducers/reducers';
+
+let store = createStore(reducer);
 
 React.render(
-  <App />,
-  document.getElementById('app')
+    // The child must be wrapped in a function
+    // to work around an issue in React 0.13.
+    <Provider store={store}>
+        {() => <App />}
+    </Provider>,
+    document.getElementById('app')
 );
