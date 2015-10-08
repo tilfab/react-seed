c13ae05bf913725ef67e69497f605d4ffb4be8a2
llernestal <lars.lernestal@gmail.com>
2015-10-08 12:05:57 +0200

    Connect App component with app state

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -1,21 +1,35 @@
 import styles from './_App.scss';
 
 import React from 'react';
+import { connect } from 'react-redux';
+import { addItem } from './actions/creators';
 import Body from '../Body/Body';
 import Footer from '../Footer/Footer';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.addItemCallback = this.addItemCallback.bind(this);
+  }
 
-  state = {
-    items: []
+  addItemCallback(text) {
+    this.props.dispatch(addItem(text));
   }
 
   render() {
     return (
       <div className={styles.app}>
-        <Body items={this.state.items} />
+        <Body items={this.props.items} />
         <Footer />
       </div>
     );
   }
 }
+
+function select(state) {
+  return {
+    items: state.items;
+  }
+}
+
+export default connect(select)(App);
