a01b0c44c109113b70820dee2ce8fb2d9c809532
llernestal <lars.lernestal@gmail.com>
2015-10-08 11:17:35 +0200

    Removed Flux

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -1,32 +1,13 @@
 import styles from './_App.scss';
 
 import React from 'react';
-import AppActions from '../../actions/AppActions';
-import ItemsStore from '../../stores/ItemsStore';
 import Body from '../Body/Body';
 import Footer from '../Footer/Footer';
 
-function getAppState() {
-  return {
-    items: ItemsStore.getAll()
-  };
-}
-
 export default class App extends React.Component {
 
-  state = getAppState()
-
-  componentDidMount() {
-    ItemsStore.addChangeListener(this.onChange);
-    AppActions.getItems();
-  }
-
-  componentWillUnmount() {
-    ItemsStore.removeChangeListener(this.onChange);
-  }
-
-  onChange = () => {
-    this.setState(getAppState());
+  state = {
+    items: []
   }
 
   render() {
